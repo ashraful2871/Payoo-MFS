c7c06b7ece754d9e0c14b6a26fea9dcca9219ae4
ashraful2871 <ashrafulislam2758@gmail.com>
2024-09-18 21:07:12 +0600

    added walet home stracture

--- js/login.js
+++ js/login.js
@@ -5,12 +5,14 @@ document
     // step-2 prevent default behavior(prevent reloding browser)
     event.preventDefault();
 
-    // step-3 get the phone number
+    // step-3 get the phone number and pin number
     const phoneNumber = document.getElementById("phone-number").value;
     const pinNumber = document.getElementById("pin-number").value;
 
-    //bad way to practice its will not in future
-    if (phoneNumber === "01759030544" && pinNumber === "2871") {
+    //step-4 validated phone and pin
+    //bad way to practice its will not in future(this is temporary i should not do like this )
+    if (phoneNumber === "12" && pinNumber === "12") {
+      //step=5 allow user to use the website
       window.location.href = "/home.html";
     } else {
       alert("your phone number and pin is incorrect");
